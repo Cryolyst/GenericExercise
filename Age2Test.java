69f983fe925805fbdac9c1ba996dea932fa0e7f2
Cryolyst <doollist@gmail.com>
2024-06-08 02:57:19 -0700

    added explanation

--- Age2Test.java
+++ Age2Test.java
@@ -1,6 +1,12 @@
 //Thant Zarni Nay (Connor) 203007421
 /*
- * Explanation here
+ * In the Age1 there are a lot of inefficiencies such as a memory inefficiency which might not be too noticible for now
+ * but if the program had a larger purpose, having that much methods that do the same thing 
+ * but the types would be super inefficient. That's why Age2 is so much more efficient as it uses generic
+ * so no type needs to be specified, saving up on a lot of space for memory. 
+ * Using generic also reduces human error as the developer don't really need to think about what kind of type it needs to be.
+ * This is also efficient in an another way that by using this method developers can code faster than writing down individual methods 
+ * for each data type. 
  */
 
 package GenericExercise;
